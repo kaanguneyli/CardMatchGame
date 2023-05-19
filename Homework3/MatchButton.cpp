6a0bcc772679e4ee8d4df800b6042f0b602b706d
Bilge Kaan Güneyli <75316186+kaanguneyli@users.noreply.github.com>
2023-05-20 02:08:41 +0300

    Update MatchButton.cpp

--- Homework3/MatchButton.cpp
+++ Homework3/MatchButton.cpp
@@ -3,10 +3,21 @@
 MatchButton::MatchButton(const int id,
                          const QString& text,
                          QWidget* parent):
-    QPushButton("?", parent)
+        QPushButton("?", parent)
+
 {
     this->text = text;
     this->id = id;
-
+    defaultColor = palette().color(QPalette::Button);
+    defaultTextColor = palette().color(QPalette::ButtonText);
 }
-
+void MatchButton::mySetEnabled(bool enabled)
+{
+    QPalette pal = palette();
+    if (! enabled) {
+        pal.setColor(QPalette::Button, defaultColor);
+        pal.setColor(QPalette::ButtonText, defaultTextColor);
+        setPalette(pal);
+    }
+    QPushButton::setEnabled(enabled);
+}
